5b83b9bbb07de78a5ea7532cca49e093000e0ab5
Zhangxingxing66 <107559133+Zhangxingxing66@users.noreply.github.com>
2025-11-04 20:19:43 +0800

    Update app.js
    Signed-off-by: Zhangxingxing66 <107559133+Zhangxingxing66@users.noreply.github.com>

--- app.js
+++ app.js
@@ -5,23 +5,21 @@ const tmpPath = require('os').tmpdir()
 const express = require('express') // ✅ 新增
 const app = express()              // ✅ 新增
 
-// ✅ 添加这个中间件来自动升级 http 图片为 https
+// ✅ 添加中间件：让浏览器自动把 http 图片升级为 https
 app.use((req, res, next) => {
-  res.setHeader('Content-Security-Policy', "upgrade-insecure-requests");
+  res.setHeader('Content-Security-Policy', 'upgrade-insecure-requests');
   next();
 });
 
 async function start() {
-  // 检测是否存在 anonymous_token 文件,没有则生成
   if (!fs.existsSync(path.resolve(tmpPath, 'anonymous_token'))) {
     fs.writeFileSync(path.resolve(tmpPath, 'anonymous_token'), '', 'utf-8')
   }
-  // 启动时更新anonymous_token
   const generateConfig = require('./generateConfig')
   await generateConfig()
   require('./server').serveNcmApi({
     checkVersion: true,
-    app, // ✅ 把上面创建的 express 实例传进去（某些版本需要）
+    app, // ✅ 将我们创建的 express 实例传给 server.js
   })
 }
 start()
